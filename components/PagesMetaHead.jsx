7c8cbb7c4047b0eec0692140126f181608a8c1ae
Eng abdalla <cllaahicabdi04@gmail.com>
2023-08-23 19:19:41 +0300

    my_protfolio

--- components/PagesMetaHead.jsx
+++ components/PagesMetaHead.jsx
@@ -1,25 +1,22 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 function PagesMetaHead({ title, keywords, description }) {
-	return (
-		<Head>
-			<meta
-				name="viewport"
-				content="width=device-width, initial-scale=1"
-			/>
-			<meta name="keywords" content={keywords} />
-			<meta name="description" content={description} />
-			<meta charSet="utf-8" />
-			<link rel="icon" href="/favicon.ico" />
-			<title>{title}</title>
-		</Head>
-	);
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>{title}</title>
+    </Head>
+  );
 }
 
 PagesMetaHead.defaultProps = {
-	title: 'Next.js & TailwindCSS Portfolio Project',
-	keywords: 'next.js, react, web, ui',
-	keywords: 'Simple and multi-page next.js and react application',
+  title: "Next.js & TailwindCSS Portfolio Project",
+  keywords: "next.js, react, web, ui",
+  keywords: "Simple and multi-page next.js and react application",
 };
 
 export default PagesMetaHead;
