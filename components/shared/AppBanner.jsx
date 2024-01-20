a522e8ee26b558975aa5129bdb08325329d795c7
Eng abdalla <cllaahicabdi04@gmail.com>
2024-01-20 11:27:48 +0300

    updated

--- components/shared/AppBanner.jsx
+++ components/shared/AppBanner.jsx
@@ -24,7 +24,7 @@ function AppBanner() {
           }}
           className="font-general-semibold text-2xl lg:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase"
         >
-          Hi, Iam Abdullahi Abdi Ahmed
+          Hi, I'M Abdullahi Abdi Ahmed
         </motion.h1>
         <motion.p
           initial={{ opacity: 0 }}
@@ -36,7 +36,7 @@ function AppBanner() {
           }}
           className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
         >
-          A Full-Stack Developer & Design Enthusiast
+          A MERN-Stack Developer & Design Enthusiast
         </motion.p>
         <motion.div
           initial={{ opacity: 0 }}
--- data/aboutMeData.js
+++ data/aboutMeData.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 export const aboutMeData = [
   {
     id: uuidv4(),
-    bio: "Hi I Am Abdullahi Abdi Ahmed Full Stack Web Developer. I am a self-taught developer with a passion for learning new things and solving problems.  and I am always looking for ways to improve my skills and knowledgee .I am a fast learner and I am not afraid to learn new things. I am always willing to help others.I am a self-motivated person and I am always looking for ways to improve myself.I am always willing to put in the extra effort to get the job done.",
+    bio: "Hi I Am Abdullahi Abdi Ahmed MERN stack  Web Developer. I am a self-taught developer with a passion for learning new things and solving problems.  and I am always looking for ways to improve my skills and knowledgee .I am a fast learner and I am not afraid to learn new things. I am always willing to help others.I am a self-motivated person and I am always looking for ways to improve myself.I am always willing to put in the extra effort to get the job done.",
   },
   {
     id: uuidv4(),
